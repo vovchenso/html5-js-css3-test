d00be259414dbcd493a80016f468e44b726b13c3
Vladimir Maluchenko <vmaluchenko@waverleysoftware.com>
2013-03-28 19:18:57 +0200

    * updates

--- js/pages/common.js
+++ js/pages/common.js
@@ -4,9 +4,49 @@ APP.PAGES = APP.PAGES || {};
 
 APP.PAGES.common = (function() {
     
+    var _createElements = function(number) {
+        var div = document.createElement('div');
+        div.className = 'item';        
+        
+        var content = APP.DOM.get('common-content');
+        content.innerHTML = '';
+        
+        var el, i;
+        for (i = 1; i <= number; i++) {
+            el = div.cloneNode();
+            el.innerHTML = '<span class="icon"></span>Test Item Element ' + i;
+            content.appendChild(el);
+        }
+    };
+    
+    var _handlers = function() {
+        APP.DOM.get('number').addEventListener('change', function() {
+            APP.DOM.get('number-text').innerHTML = this.value;
+            _createElements(this.value);
+        });
+        
+        var btns = APP.DOM.queryAll('button.btn');
+        for (var btn in btns) {
+            btns[btn].addEventListener('click', function() {
+                this.classList.toggle('active');
+                
+                var cls = this.getAttribute('data-class');
+                var items = APP.DOM.queryAll('.item');
+                for (var item in items) {
+                    items[item].classList.toggle(cls);
+                }
+            });
+        }
+    };
+    
+    var _init = function() {
+        _createElements(25);
+    };
+    
     var module = {
         init: function() {
-            
+            _init();
+            _handlers();
         }
     };
     
